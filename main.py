58216f7b600c92286a24a92a0b2e0c56c620f9b7
stefanociccolella <sciccolella2018@fau.edu>
2022-10-06 23:54:18 -0400

    Proof of concept completed

--- main.py
+++ main.py
@@ -15,16 +15,24 @@ from multiprocessing import Process
 import threading
 import uvicorn
 from start_camera import start_camera
+import serial
 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 kill_cam = 0
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
 
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.htm", context={"request": request})
 
+@app.get("/esp")
+async def write_read(position):
+    arduino.write(bytes(position, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
 
 # TODO bring USB ethernet dongle
 
@@ -36,4 +44,3 @@ if __name__ == '__main__':
     p.start()
     uvicorn.run(app, host="0.0.0.0", port=8000, access_log=False)
     p.close()
-    print('started')
